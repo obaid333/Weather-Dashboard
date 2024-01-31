137544c6faab063e5bd41ac275c802dc0f1e060c
Obaid Gulzar <obaid3337@googlemail.com>
2024-01-31 07:27:32 +0000

    linked time and date to data.dt

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,6 @@
 const apiKey = "99770420a6c96e9be744dc6bef0c1618";
 var city = "Liverpool";
-//var lat = "";
-//var lon = "";
-// var coordsApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" +city+ "&limit=1&appid=" +apiKey;
-//var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" +lat+ "&lon=" +lon+ "&appid=" +apiKey+ "&units=metric";
-//var currentDataUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" +lat+ "&lon=" +lon+ "&appid=" +apiKey+ "&units=metric";
-//console.log(coordsApiUrl);
+
 
 
 $("#search-addon").on("click", function() {
@@ -21,7 +16,7 @@ fetch(coordsApiUrl)
   .then(function (response) {
     return response.json();
   }).then(function (data) {
-    console.log(data);
+    //console.log(data);
     var lat = data[0].lat;
     var lon = data[0].lon;
     
@@ -31,7 +26,7 @@ fetch(coordsApiUrl)
     .then(function (response) {
       return response.json();
     }).then(function (data) {
-        //console.log(data);
+        console.log(data);
     });
 
     var currentDataUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" +lat+ "&lon=" +lon+ "&appid=" +apiKey+ "&units=metric";
@@ -40,10 +35,11 @@ fetch(coordsApiUrl)
       return response.json();
     }).then(function (data) {
         console.log(data);
+        
         $("#currentTemp").text(data.main.temp + "C");
         $("#cityName").text(data.name);
-        $("#currentDate").text(dayjs().format('DD/MM/YYYY'));
-        $("#currentTime").text(dayjs().format('HH:MM'));
+        $("#currentDate").text(dayjs(data.dt * 1000).format('DD/MM/YYYY'));
+        $("#currentTime").text("Last Updated: " +dayjs(data.dt * 1000).format('HH:MM'));
         $("#messageMain").text(data.weather[0].description);
         $("#windSpeedMain").text(data.wind.speed + "KPH");
         $("#humidityMian").text(data.main.humidity + "%");
